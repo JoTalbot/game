7a379cc0e0a1c4b6b2f2b5e45815ebd2ab97d763
JoTalbot <jo.talbot@gmail.com>
2026-08-17 19:47:34 +0000

    релиз 1.6.6 код 5/10

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "world.igra.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 165
-        versionName = "1.6.5"
+        versionCode = 166
+        versionName = "1.6.6"
     }
 
     buildTypes {
