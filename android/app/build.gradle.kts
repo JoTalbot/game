d84024942ee69376b10f5b674bd23c6edebb9304
JoTalbot <jo.talbot@gmail.com>
2026-08-17 19:50:41 +0000

    релиз 1.8.4 код 3/10

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "world.igra.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 183
-        versionName = "1.8.3"
+        versionCode = 184
+        versionName = "1.8.4"
     }
 
     buildTypes {
