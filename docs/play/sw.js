89913da851add3d9036a8b8219dc87544088a527
JoTalbot <JoTalbot@users.noreply.github.com>
2026-09-02 23:03:07 +0300

    fix(v3-004): sync Pages cache

--- docs/play/sw.js
+++ docs/play/sw.js
@@ -3,7 +3,7 @@ var CACHE = "igra-shell-v3";
 var ASSETS = [
   "./", "./index.html", "./manifest.json", "./assets/img/icon.png",
   "./assets/fonts/f0.ttf", "./assets/fonts/f1.ttf", "./assets/fonts/f2.ttf", "./assets/fonts/f3.ttf", "./assets/fonts/f4.ttf", "./assets/fonts/f5.ttf", "./assets/fonts/f6.ttf", "./assets/fonts/f7.ttf", "./assets/fonts/f8.ttf",
-  "./css/fonts.css", "./css/game.css", "./js/math.js", "./js/lang.js", "./js/dna.js", "./js/save.js", "./js/audio.js", "./js/fx.js", "./js/igra.js", "./js/report.js", "./js/organs.js", "./js/memory.js", "./js/fate.js", "./js/world.js", "./js/director.js", "./js/life.js", "./js/relationships.js", "./js/renderer.js", "./js/webgl-renderer.js", "./js/engine.js", "./js/ui.js", "./js/main.js"
+  "./css/fonts.css", "./css/game.css", "./js/math.js", "./js/lang.js", "./js/dna.js", "./js/save.js", "./js/audio.js", "./js/fx.js", "./js/igra.js", "./js/report.js", "./js/organs.js", "./js/memory.js", "./js/fate.js", "./js/world.js", "./js/director.js", "./js/life.js", "./js/relationships.js", "./js/world-memory.js", "./js/renderer.js", "./js/webgl-renderer.js", "./js/engine.js", "./js/ui.js", "./js/main.js"
 ];
 self.addEventListener("install", function (event) { event.waitUntil(caches.open(CACHE).then(function (cache) { return cache.addAll(ASSETS); })); });
 self.addEventListener("activate", function (event) { event.waitUntil(caches.keys().then(function (keys) { return Promise.all(keys.filter(function (key) { return key !== CACHE; }).map(function (key) { return caches.delete(key); })); })); });
